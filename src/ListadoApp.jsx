30df9b51f8e3b99528e22b4999b9745b7d01d222
Hency Starlin Lara Brea <hency.lara@inapa.gob.do>
2026-04-27 14:52:42 -0400

    clase map

--- src/ListadoApp.jsx
+++ src/ListadoApp.jsx
@@ -1,22 +1,39 @@
+import { useState } from "react"
+
 const Items = ({descripcion, visto}) => {
   return(<li>{descripcion} {visto ? "✅" : "⛔"} </li>)
 }
 
 export const ListadoApp = () => {
+
+  let listado = [
+              {descripcion: "Una cosa", visto: false },
+              {descripcion: "Una cosa", visto: false },
+              {descripcion: "Una cosa", visto: false },
+              {descripcion: "Una cosa", visto: true },
+              {descripcion: "Una cosa", visto: false },
+              {descripcion: "Una cosa", visto: false },
+              {descripcion: "Una cosa", visto: false },
+              {descripcion: "Una cosa", visto: true },
+              {descripcion: "Una cosa", visto: false },
+              {descripcion: "Una cosa", visto: false },
+              {descripcion: "Una cosa", visto: false },
+  ]
+
+  const [arreglo, setArreglo] = useState(listado);
+
+  const handleClick = () => {
+    console.log(arreglo);
+    setArreglo([...arreglo, {descripcion: "Cambiando la cosa", visto: true}]);
+  }
+
   return (
     <>
     <h1>Listado de Temas</h1>
-      <Items descripcion = "Una cosa" visto = {false }/>
-      <Items descripcion = "Una cosa" visto = {false }/>
-      <Items descripcion = "Una cosa" visto = {false }/>
-      <Items descripcion = "Una cosa" visto = {true }/>
-      <Items descripcion = "Una cosa" visto = {false }/>
-      <Items descripcion = "Una cosa" visto = {false }/>
-      <Items descripcion = "Una cosa" visto = {false }/>
-      <Items descripcion = "Una cosa" visto = {true }/>
-      <Items descripcion = "Una cosa" visto = {false }/>
-      <Items descripcion = "Una cosa" visto = {false }/>
-      <Items descripcion = "Una cosa" visto = {false }/>
+      <ol>
+        {arreglo.map(item => <Items key={item.visto} descripcion={item.descripcion} visto={item.visto} ></Items>)}
+      </ol>
+      <button onClick={handleClick}>Agregar al listado</button>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-<<<<<<< HEAD
-import { Tarjeta } from "./ejercicios/Tarjeta";
-// import { Lista } from "./ejercicios/Lista";
-import { BorrarMap } from "./ejercicios/BorrarMap";
-=======
 import {TextoColor} from "./ejercicios/TextoColor"
->>>>>>> 495c7f2bde44cf1b52c55288f11218942f81503d
 import "./main.css";
 import { Saludar } from "./ejercicios/Saludar";
+import { ListadoApp } from "./ListadoApp";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-<<<<<<< HEAD
-    {/* <Tarjeta nombre="hency lara" edad={55} profesion="vago"></Tarjeta>
-    <Lista
-      tareas={[
-        { id: 1, texto: "Aprender React", done: true },
-        { id: 2, texto: "Hacer ejercicios", done: false },
-        { id: 3, texto: "Leer documentación de Tailwind", done: false },
-        { id: 4, texto: "Configurar proyecto en GitHub", done: true },
-        { id: 5, texto: "Practicar los pilares de OOP", done: false },
-        { id: 6, texto: "Escribir correo para el anteproyecto", done: true },
-        { id: 7, texto: "Sincronizar Firebase Studio", done: false },
-        { id: 8, texto: "Analizar acordes musicales", done: true },
-        { id: 9, texto: "Revisar validaciones con regex", done: false },
-        { id: 10, texto: "Preparar presentación académica", done: false },
-      ]}
-    ></Lista> */}
-    <BorrarMap />
-=======
   
-    <TextoColor texto= "Hola mundo" color = "red" />
-    <Saludar nombre="Prueba"/>
->>>>>>> 495c7f2bde44cf1b52c55288f11218942f81503d
+   <ListadoApp />
   </StrictMode>,
 );
