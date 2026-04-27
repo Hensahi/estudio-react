18b120a8e993cbbf5ad90d5b01eb81a8cc7551c0
Hency Starlin Lara Brea <hency.lara@inapa.gob.do>
2026-04-27 14:09:56 -0400

    se arreglan conflictos entre ramas

--- src/ejercicios/Lista.jsx
+++ src/ejercicios/Lista.jsx
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-// export const Lista = ({ tareas }) => {
-//   return tareas.map((tarea) => {tarea.done ? <li>{tarea.texto "✅"} </li> : <li> {tarea.texto "⛔"} </li>});
-// };
-=======
 
 
 
@@ -15,4 +10,3 @@ const listItems = tareas.map(tarea => <li key={tarea.id}>
 return <ul>{listItems}</ul>
 };
 
->>>>>>> 495c7f2bde44cf1b52c55288f11218942f81503d
